c37695bb5503a16449fe749722cda5199facb259
Jinho Jang <verycosy@kakao.com>
2023-01-16 16:19:19 +0900

    react-native-webview 설치

--- apps/rnapp/src/app/App.tsx
+++ apps/rnapp/src/app/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
-import { StatusBar, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import WebView from 'react-native-webview';
 
 const Stack = createStackNavigator();
+
+const WebViewScreen: React.FC = () => {
+  return (
+    <WebView
+      source={{
+        uri: 'https://google.com',
+      }}
+    />
+  );
+};
 
 export const App = () => {
   return (
@@ -11,14 +22,7 @@ export const App = () => {
       <StatusBar barStyle="dark-content" />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen
-            name="Sample"
-            component={() => (
-              <View>
-                <Text>Good</Text>
-              </View>
-            )}
-          />
+          <Stack.Screen name="WebViewScreen" component={WebViewScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
