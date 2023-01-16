2fd520786ed3d7e94a85e41c8bed420e4fabb77e
Jinho Jang <verycosy@kakao.com>
2023-01-16 16:07:44 +0900

    react-navigation stack 설치

--- apps/rnapp/src/app/App.tsx
+++ apps/rnapp/src/app/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView></SafeAreaView>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Sample"
+            component={() => (
+              <View>
+                <Text>Good</Text>
+              </View>
+            )}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 };
